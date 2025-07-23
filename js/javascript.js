cb8726efad0cc8b684124842c5e014286217e774
J3r3m16h <jeremiahmwaura377@gmail.com>
2025-07-23 10:29:11 +0300

    portfolio

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+/*========================= typing animation =====================*/
+var typed = new Typed(".typing",{
+    strings:[" ","Cyber Security Student", "Web Developer", "Graphics Designer", "Business Man"],
+    typeSpeed:100,
+    BackSpeed:160,
+    loop:true
+})
